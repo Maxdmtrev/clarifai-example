69c03d4bf69de2fd12ce3c795b2eb5b19e2cfa4e
Max Dmitriev <maxdmtrev@gmail.com>
2020-04-10 20:04:23 +0300

    Refactoring index.js

--- app.js
+++ app.js
@@ -8,25 +8,20 @@ const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const app = express();
-// const clarifai = new Clarifai.App({  apiKey: 'd6dd707fbdd9404e869be038368df4db'});
 const port = 3000;
-// const upload = multer();
 
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
         const method = req.body._method;
         delete req.body._method;
         return method;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-
 const express = require("express");
 const router = express.Router();
-const Clarifai = require("clarifai");
-const path = require('path');
-const bodyParser = require("body-parser");
-const clarifai = new Clarifai.App({  apiKey: 'd6dd707fbdd9404e869be038368df4db'});
 
+const Clarifai = require("clarifai");
+const clarifai = new Clarifai.App({  apiKey: 'd6dd707fbdd9404e869be038368df4db'});
 
 router.route('/')
     .get(async (req, res, next) => {
@@ -30,25 +27,33 @@ router.route('/upload')
             console.log(err);
         });
       } if(inputModel === 'demograph') {
-                await clarifai.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", `${url}`)
+          await clarifai.models.predict("c0c0ac362b03416da06ab3fa36fb58e3", `${url}`)
             .then(response => {
-                let age = response['outputs'][0]['data']['regions'][0]['data']['face']['age_appearance']['concepts'][0];
-                let age1 = response['outputs'][0]['data']['regions'][0]['data']['face']['age_appearance']['concepts'][1];
-                let nameAge = age.name;
-                let valueAge = age.value.toFixed(3);
-                let nameAge1 = age1.name;
-                let valueAge1 = age1.value.toFixed(3);
-                let gender_fem = response['outputs'][0]['data']['regions'][0]['data']['face']['gender_appearance']['concepts'][0];
-                let gender_mas = response['outputs'][0]['data']['regions'][0]['data']['face']['gender_appearance']['concepts'][1];
-                let genderName = gender_fem.name;
-                let genderValue = gender_fem.value.toFixed(3);
-                let genderName1 = gender_mas.name;
-                let genderValue1 = gender_mas.value.toFixed(3);
-                let result = response['outputs'][0]['data']['regions'][0]['data']['face']['multicultural_appearance']['concepts'];
-                let nameMulti = result.map(e => e.name);
-                let valueMulti = result.map(e => e.value.toFixed(3));
-                 res.render('demography', {nameAge, valueAge, nameAge1, valueAge1,
-                                           genderName, genderName1, genderValue, genderValue1, result, nameMulti, valueMulti, url} )
+              const data = response['outputs'][0]['data']['regions'][0]['data']['face']['age_appearance']['concepts'];
+              const result = response['outputs'][0]['data']['regions'][0]['data']['face']['multicultural_appearance']['concepts'];
+
+              const age = data[0];
+              const ageValue = data[1];
+              const gender_fem = data[0];
+              const gender_mas = data[1];
+
+              let nameAge = age.name;
+              let nameAge1 = ageValue.name;
+
+              let genderName = gender_fem.name;
+              let genderName1 = gender_mas.name;
+
+              let valueAge = age.value.toFixed(3);
+              let valueAge1 = ageValue.value.toFixed(3);
+              let genderValue = gender_fem.value.toFixed(3);
+              let genderValue1 = gender_mas.value.toFixed(3);
+
+              let nameMulti = result.map(e => e.name);
+              let valueMulti = result.map(e => e.value.toFixed(3));
+               res.render('demography', {
+                 nameAge, valueAge, nameAge1, valueAge1, genderName, genderName1,
+                 genderValue, genderValue1, result, nameMulti, valueMulti, url
+               })
             })
             .catch(err => {
                 console.log(err);
